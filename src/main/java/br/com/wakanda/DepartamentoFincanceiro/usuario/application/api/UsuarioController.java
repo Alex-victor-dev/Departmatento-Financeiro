de197b8f1831ec58b1a866b457000a9d4aa97702
Oscar-devjava <negociante2t@gmail.com>
2022-08-26 06:25:35 -0300

    ajusta nomenclatura em usuario service e controller

--- src/main/java/br/com/wakanda/DepartamentoFincanceiro/usuario/application/api/UsuarioController.java
+++ src/main/java/br/com/wakanda/DepartamentoFincanceiro/usuario/application/api/UsuarioController.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.wakanda.DepartamentoFincanceiro.usuario.service.ClienteService;
+import br.com.wakanda.DepartamentoFincanceiro.usuario.application.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -13,12 +13,12 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 public class UsuarioController implements UsuarioAPI {
 
-	private final ClienteService clienteService;
+	private final UsuarioService usuarioService;
 
 	@Override
 	public UsuarioResponse postAdicionaNovoUsuario(UsuarioRequest usuarioRequest) {
 		log.info("[inicia] UsuarioController - postAdicionaNovoUsuario ");
-		UsuarioResponse usuarioNovo = clienteService.usuarioCriado(usuarioRequest);
+		UsuarioResponse usuarioNovo = usuarioService.usuarioCriado(usuarioRequest);
 		log.info("[finaliza] UsuarioController - postAdicionaNovoUsuario ");
 		return usuarioNovo;
 	}
--- src/main/java/br/com/wakanda/DepartamentoFincanceiro/usuario/application/service/UsuarioService.java
+++ src/main/java/br/com/wakanda/DepartamentoFincanceiro/usuario/application/service/UsuarioService.java
@@ -1,9 +1,9 @@
-package br.com.wakanda.DepartamentoFincanceiro.usuario.service;
+package br.com.wakanda.DepartamentoFincanceiro.usuario.application.service;
 
 import br.com.wakanda.DepartamentoFincanceiro.usuario.application.api.UsuarioRequest;
 import br.com.wakanda.DepartamentoFincanceiro.usuario.application.api.UsuarioResponse;
 
-public interface ClienteService {
+public interface UsuarioService {
 
 	UsuarioResponse usuarioCriado(UsuarioRequest usuarioRequest);
 
